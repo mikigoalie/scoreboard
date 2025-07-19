236837ba5868b7e15fb1148eb78b97d517a51d1d
mikigoalie <mikigoalie@seznam.cz>
2025-07-19 15:01:04 +0200

    Update index.jsx
    remove mantine colors and use internal color functions via badge component

--- web/src/SocietyList/SocietyComponent/index.jsx
+++ web/src/SocietyList/SocietyComponent/index.jsx
@@ -5,14 +5,12 @@ import { UsersIcon } from '../../utils/icons';
 const playerName = "TEST";
 const playerServerId = 3;
 const SocietyComponent = ({ label, playerCount }) => {
-	const color = playerCount > 0 ? 'var(--mantine-primary-color-light)' : 'var(--mantine-color-red-light)';
-	const textColor = playerCount > 0 ? 'var(--mantine-primary-color-light-color)' : 'var(--mantine-color-red-light-color)';
+	const color = playerCount > 0 ? undefined : 'red';
 	return (
 		<Group bg="dark.6" pl={4} >
 			<Text >{label}</Text>
-			<Badge ml="auto" h="2vw" mih={32} miw={86} p={0} m={0} px={8} radius="xs" variant="filled" color={color} leftSection={<UsersIcon
-				color={textColor} size={14} />}>
-				<Text c={textColor} fz={12}>{playerCount}</Text>
+			<Badge ml="auto" h="2vw" mih={32} miw={86} p={0} m={0} px={8} radius="xs" variant="light" color={color} leftSection={<UsersIcon size={14} />}>
+				{playerCount}
 			</Badge>
 		</Group>
 	);
