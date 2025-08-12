15990c6a35e0b0bc290ea9d954e6ed03588aff86
mikigoalie <mikigoalie@seznam.cz>
2025-08-12 17:43:49 +0200

    Update vite.config.js
    add visualizer

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({
+  plugins: [visualizer(), react({
     babel: {
       plugins: [['babel-plugin-react-compiler', { target: '19' }]],
     },
